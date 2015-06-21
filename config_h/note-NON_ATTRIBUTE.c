658765ba3df3c27a3f4df5728605d105e170a264
Cody P Schafer <dev@codyps.com>
2015-06-20 23:54:52 -0400

    fix note inversion

--- config_h/note-NON_ATTRIBUTE.c
+++ config_h/note-NON_ATTRIBUTE.c
@@ -1,5 +1,5 @@
-/* FAILED: compiler does not emit an error for unrecognized attributes, config.h definitions may be incorrect */
-/* SUCCEEDED: compiler errors when unrecognized attributes are used, no effect on config.h accuracy */
+/* SUCCEEDED: compiler does not emit an error for unrecognized attributes, config.h definitions may be incorrect */
+/* FAILED: compiler errors when unrecognized attributes are used, no effect on config.h accuracy */
 __attribute__((some_fake_attribute))
 int main(void)
 {
